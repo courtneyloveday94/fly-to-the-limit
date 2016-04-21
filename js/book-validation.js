59c1a0cafbc262a02fccf7416cd323d0acfec83e
courtney sarah loveday <845862270@kent-202-12m-2.local>
2016-04-22 09:59:28 +1200

    cleaned up html and css after attempting to validate

--- js/book-validation.js
+++ js/book-validation.js
@@ -1,112 +1,218 @@
 // find the form
-var bookingForm = document.querySelector('#book');
+var bookingForm = document.querySelector('#book-now-form');
 
-console.log("i hope this works")
+console.log('hello?')
 
-// wait for the user to submit the form
-bookingForm.onsubmit = function(event) {
 
-	// count how many errors there are
-	var totalErrors = 0;
-
+// count how many errors there are
+var totalErrors = 0;
 	// get references to the input fields
-	var nameInput = document.querySelector("#name");
-	var nameMessage = document.querySelector("#name-message");
+var nameInput = document.querySelector("#name");
+var nameMessage = document.querySelector("#name-message");
 
-	var emailInput = document.querySelector("#email");
-	var emailMessage = document.querySelector("#email-message");
+var emailInput = document.querySelector("#email");
+var emailMessage = document.querySelector("#email-message");
 
-	var phoneInput = document.querySelector("#phone");
-	var phoneMessage = document.querySelector("#phone-message");
+var phoneInput = document.querySelector("#phone");
+var phoneMessage = document.querySelector("#phone-message");
 
-	var flightMessage = document.querySelector("#flight-message");
-	var flightOptions = document.querySelectorAll("[name=flight]");
+var flightMessage = document.querySelector("#flight-message");
+var flightOptions = document.querySelectorAll("[name=flight]");
 
-	var aircraftMessage = document.querySelector("#aircraft-message");
-	var aircraftOptions = document.querySelectorAll("[name=aircraft]");
+var aircraftMessage = document.querySelector("#aircraft-message");
+var aircraftOptions = document.querySelectorAll("[name=aircraft]");
 
-	var passenegersInput = document.querySelector("#passengers");
-	var passengersMessage = document.querySelector("#passengers-message");
+var passengersInput = document.querySelector("#passengers");
+var passengersMessage = document.querySelector("#passengers-message");
 
-	var locationInput = document.querySelector("#location");
-	var locationMessage = document.querySelector("#location-message");
+var locationInput = document.querySelector("#location");
+var locationMessage = document.querySelector("#location-message");
 
-	var dateInput = document.querySelector("#date");
-	var dateMessage = document.querySelector("#date-message");
+var dateInput = document.querySelector("#date");
+var dateMessage = document.querySelector("#date-message");
 
-	var timeInput = document.querySelector("#time");
-	var timeMessage = document.querySelector("#time-message");
+var timeInput = document.querySelector("#time");
+var timeMessage = document.querySelector("#time-message");
 
-	var dropOffLocationInput = document.querySelector("#drop-off-location");
-	var dropOffLocationMessage = document.querySelector("#drop-off-location-message");
+var dropOffLocationInput = document.querySelector("#drop-off-location");
+var dropOffLocationMessage = document.querySelector("#drop-off-location-message");
 
-	var dropOffDateInput = document.querySelector("#drop-off-date");
-	var dropOffDateMessage = document.querySelector("#drop-off-date-message");
+var dropOffDateInput = document.querySelector("#drop-off-date");
+var dropOffDateMessage = document.querySelector("#drop-off-date-message");
 
-	var dropOffTimeInput = document.querySelector("#drop-off-time");
-	var dropOffTimeMessage = document.querySelector("#drop-off-time-message");
+var dropOffTimeInput = document.querySelector("#drop-off-time");
+var dropOffTimeMessage = document.querySelector("#drop-off-time-message");
 
-	var pickUpLocationInput = document.querySelector("#pick-up-location");
-	var pickUpLocationMessage = document.querySelector("#pick-up-location-message");
+var pickUpLocationInput = document.querySelector("#pick-up-location");
+var pickUpLocationMessage = document.querySelector("#pick-up-location-message");
 
-	var pickUpDateInput = document.querySelector("#pick-up-date");
-	var pickUpDateMessage = document.querySelector("#pick-up-date-message");
+var pickUpDateInput = document.querySelector("#pick-up-date");
+var pickUpDateMessage = document.querySelector("#pick-up-date-message");
 
-	var pickUpTimeInput = document.querySelector("#pick-up-time");
-	var pickUpTimeMessage = document.querySelector("#pick-up-time-message");
+var pickUpTimeInput = document.querySelector("#pick-up-time");
+var pickUpTimeMessage = document.querySelector("#pick-up-time-message");
 
 
-	// check the name 
-	if(namePattern.test(nameInput.value)) {
-		nameMessage.innerHTML = "";
-		
+// check the name 
+if(namePattern.test(nameInput.value)) {
+	nameMessage.innerHTML = "";
+} else {
+	//name is invalid
+	nameMessage.innerHTML = "Name is invalid.";
+	totalErrors++
+}
+
+// email
+if(emailPattern.test(emailInput.value)) {
+	emailMessage.innerHTML = "";
+} else {
+	//email is invalid
+	emailMessage.innerHTML = "Email is invalid.";
+	totalErrors++
+}
+
+// phone
+if(phonePattern.test(phoneInput.value)) {
+	phoneMessage.innerHTML = "";
+} else {
+	//phone is invalid
+	phoneMessage.innerHTML = "Phone number is invalid.";
+	totalErrors++
+}
+
+// flights
+// loop over all the flight options
+var flightIsSelected = false;
+	for(var i=0; i<flightOptions.length; i++) {
+	// check if this flight has been selected
+	if(flightOptions[i].checked) {
+		flightIsSelected = true;
 	}
+}
+// if flightIsSelected is still false
+if(flightIsSelected == false) {
+	flightMessage.innerHTML = "Please select a flight.";
+	totalErrors++;
+} else {
+	flightMessage.innerHTML = "";
+}
 
+// aircraft
+// loop over all the aircraft options
+var aircraftIsSelected = false;
+	for(var i=0; i<aircraftOptions.length; i++) {
+	// check if this aircraft has been selected
+	if(aircraftOptions[i].checked) {
+		aircraftIsSelected = true;
+	}
+}
+// if aircraftIsSelected is still false
+if(aircraftIsSelected == false) {
+	aircraftMessage.innerHTML = "Please select a aircraft.";
+	totalErrors++;
+} else {
+	aircraftMessage.innerHTML = "";
+}
 
-	// // check the username
-	// if(usernamePattern.test(usernameInput.value)) {
-	// 	// username is valid
-	// 	usernameMessage.innerHTML = "";
-	// } else {
-	// 	// username is invalid
-	// 	usernameMessage.innerHTML = "Username is invalid.";
-	// 	totalErrors++;
-	// }
+// passengers
+if(passengersPattern.test(passengersInput.value)) {
+	passengersMessage.innerHTML = "";
+} else {
+	//passengers is invalid
+	passengersMessage.innerHTML = "Passengers is invalid.";
+	totalErrors++
+}
 
-	// if(namePattern.test(nameInput.value)) {
-	// 	nameMessage.innerHTML = "";
-	// } else {
-	// 	nameMessage.innerHTML = "Name is invalid.";
-	// 	totalErrors++;
-	// }
+// location
+if(locationPattern.test(locationInput.value)) {
+	locationMessage.innerHTML = "";
+} else {
+	//location is invalid
+	locationMessage.innerHTML = "Location is invalid.";
+	totalErrors++
+}
 
-	// // loop over all the campus options
-	// var campusIsSelected = false;
+// date
+if(datePattern.test(dateInput.value)) {
+	dateMessage.innerHTML = "";
+} else {
+	//date is invalid
+	dateMessage.innerHTML = "Date is invalid.";
+	totalErrors++
+}
 
-	// for(var i=0; i<campusOptions.length; i++) {
-	// 	// check if this campus has been selected
-	// 	if(campusOptions[i].checked) {
-	// 		campusIsSelected = true;
-	// 	}
-	// }
+// time
+if(timePattern.test(timeInput.value)) {
+	timeMessage.innerHTML = "";
+} else {
+	//time is invalid
+	timeMessage.innerHTML = "Time is invalid.";
+	totalErrors++
+}
 
-	// // if campusIsSelected is still false
-	// if(campusIsSelected == false) {
-	// 	campusMessage.innerHTML = "Please select a campus.";
-	// 	totalErrors++;
-	// } else {
-	// 	campusMessage.innerHTML = "";
-	// }
+// drop off location
+if(dropOffLocationPattern.test(dropOffLocationInput.value)) {
+	dropOffLocationMessage.innerHTML = "";
+} else {
+	//dropOffLocation is invalid
+	dropOffLocationMessage.innerHTML = "Drop off location is invalid.";
+	totalErrors++
+}
 
-	// // if the total errors are greater than 0
-	// if(totalErrors > 0) {
-	// 	// stop the form from submiting
-	// 	event.preventDefault();
-	// }	
+// drop off date
+if(dropOffDatePattern.test(dropOffDateInput.value)) {
+	dropOffDateMessage.innerHTML = "";
+} else {
+	//drop off date is invalid
+	dropOffDateMessage.innerHTML = "Drop off date is invalid.";
+	totalErrors++
+}
 
-	bookingForm.onsubmit = function(event) {
-		//stops the form from submitting
+// drop off time
+if(dropOffTimePattern.test(dropOffTimeInput.value)) {
+	dropOffTimeMessage.innerHTML = "";
+} else {
+	//Drop off time is invalid
+	dropOffTimeMessage.innerHTML = "Drop off Time is invalid.";
+	totalErrors++
+}
+
+// pick up location
+if(pickUpLocationPattern.test(pickUpLocationInput.value)) {
+	pickUpLocationMessage.innerHTML = "";
+} else {
+	// pick up location is invalid
+	pickUpLocationMessage.innerHTML = "Pick up location is invalid.";
+	totalErrors++
+}
+
+// pick up date
+if(pickUpDatePattern.test(pickUpDateInput.value)) {
+	pickUpDateMessage.innerHTML = "";
+} else {
+	// pick up date is invalid
+	pickUpDateMessage.innerHTML = "Pick up date is invalid.";
+	totalErrors++
+}
+
+// pick up time
+if(pickUpTimePattern.test(pickUpTimeInput.value)) {
+	pickUpTimeMessage.innerHTML = "";
+} else {
+	// pick up time is invalid
+	pickUpTimeMessage.innerHTML = "Pick up time is invalid.";
+	totalErrors++
+}
+
+// if the total errors are greater than 0
+if(totalErrors > 0) {
+	// stop the form from submiting
+	event.preventDefault();
+	console.log('you have errors')
+}
+
+//If the total errors is greater than 0
+	if( totalErrors > 0 ) {
+		// Stop the form from submitting
 		event.preventDefault();
 	}
-
-}
--- js/patterns.js
+++ js/patterns.js
@@ -3,7 +3,7 @@
 	// for book-validation.js
 	var namePattern = new RegExp("^[a-zA-Z '.-]{1,30}$");
 	var emailPattern = new RegExp("^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i");
-	var phonePattern = new RegExp("^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$")
+	var phonePattern = new RegExp("[0-9+()/-]{3,50}")
 	var passengersPattern = new RegExp("^[0-9]{1,2}$")
 	var locationPattern = new RegExp("[0-9a-zA-Z./-]{1-50}")
 	var datePattern = new RegExp("^(0?[ 1-9]|[12][0-9]|3[01])[\/\-](0?[ 1-9]|1[012])[\/\-]\d{4}$")
